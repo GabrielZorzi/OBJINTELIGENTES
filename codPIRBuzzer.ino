ca57ebb79ac6da60fb5b342a70f335495512a552
GabrielZorzi <75396203+GabrielZorzi@users.noreply.github.com>
2022-06-14 15:32:10 -0300

    Update codPIRBuzzer.ino

--- codPIRBuzzer.ino
+++ codPIRBuzzer.ino
@@ -1,22 +1,127 @@
-void setup()
-{
-  pinMode(2,INPUT);
-  pinMode(8, OUTPUT);
-  Serial.begin(9600);
+
+#include "DHT.h"
+#include <ESP8266WiFi.h> //inclui suporte ao NodeMCU
+#include <PubSubClient.h> //inclui suporte ao MQTT no HiveMQ Cloud
+
+// Configuracoes da rede WiFi
+const char* ssid = "Alex"; //SSID da rede WiFi
+const char* password = "19315400"; //senha da rede WiFi
+
+const char* mqtt_server = "98571f1b42354f9da3be4ff37ad39065.s1.eu.hivemq.cloud"; //URL do broker MQTT
+const int mqtt_server_port = 8883; //porta do broker MQTT
+
+// Variaveis globais e objetos
+#define MSG_BUFFER_SIZE (500) //define MSG_BUFFER_SIZE como 500
+#define DHTPIN D7 // pino que estamos conectado
+#define LED D7
+WiFiClient client; //cria o objeto client
+PubSubClient mqtt_client(client); //cria o objeto mqtt_client
+long lastMsg = 0;
+
+String clientID = "ESP8266Client-"; //identificacao do cliente
+
+String topicoPrefixo = "MACK32054531"; //para o prefixo do topico, utilizar MACK seguido do TIA
+String topicoTodos = topicoPrefixo + "/#"; //para retornar todos os topicos
+String topico_0 = topicoPrefixo + "/hello"; //topico para teste
+String mensagem_0 = "NodeMCU Connected"; //mensagem para o topico 0
+String topico_1 = topicoPrefixo + "/sensor1"; //topico para o sensor 1
+String mensagem_1 = ""; //mensagem para o topico 1
+String topico_2 = topicoPrefixo + "/atuador1"; //topico para o atuador 1
+String mensagem_2 = ""; //mensagem para o topico 2
+String mensagemTemp = ""; //armazena temporariamente as mensagens recebidas via MQTT
+
+
+DHT dht(DHTPIN, DHTTYPE);
+
+// Funcao para configurar a conexao WiFi
+void setup_wifi() {
+  //WiFi.mode(WIFI_STA); //ativa o modo de estacao
+  WiFi.begin(ssid, password);
+  Serial.print("Connecting to ");
+  Serial.println(ssid);
+
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+
+    Serial.println();
+    Serial.print("Connected, IP address: ");
+    Serial.println(WiFi.localIP());
+  }
+
+  // Funcao para receber mensagens
+void callback(char* topic, byte* payload, unsigned int length) {
+  mensagemTemp = "";
+  Serial.print("Message arrived [");
+  Serial.print(topic);
+  Serial.print("] ");
+  for (int i = 0; i < length; i++) {
+    Serial.print((char)payload[i]);
+    mensagemTemp += (char)payload[i];
+  }
+
+  if (String(topic) == topico_2) {
+    mensagem_2 = mensagemTemp;
+  }
+
+  Serial.println();
+
+  Serial.println();
+  Serial.print("Connected, IP address: ");
+  Serial.println(WiFi.localIP());
 }
 
-void loop()
+// Funcao para conectar no broker MQTT
+void reconnect() {
+  // Loop until we’re reconnected
+  while (!mqtt_client.connected()) {
+    Serial.print("Attempting MQTT connection…");
+
+  // Create a random client ID
+    randomSeed(micros()); //inicializa a semente do gerador de numeros aleatorios
+    clientID += String(random(0xffff), HEX);
+
+  // Attempt to connect
+    if (mqtt_client.connect(clientID.c_str())) {
+      Serial.println("connected");
+      // Once connected, publish an announcement...
+      mqtt_client.publish(topico_0.c_str(), mensagem_0.c_str());
+      // ... and resubscribe
+      mqtt_client.subscribe(topicoTodos.c_str());
+    } else {
+      Serial.print("failed, rc=");
+      Serial.print(mqtt_client.state());
+      Serial.println(" try again in 5 seconds");
+      // Wait 5 seconds before retrying
+      delay(5000);
+    }
+  }
+} 
+
+
+
+int pir = 0;
+
+void setup()
 {
-  if(digitalRead(2)==HIGH)
-  {
-    Serial.println("MOVIMENTO ");
-    digitalWrite(8,HIGH);
-    delay(200);
-    digitalWrite(8,LOW);
-    delay(100);
+  pinMode(A0, INPUT);
+  Serial.begin(9600);
+  pinMode(3, OUTPUT);
+  setup_wifi();
+  mqqt_client.setServer(mqtt_server, mqtt_server_port);
+  mqtt_client.setCallback(callback);
+  
+}
+
+void loop(){
+  if (!mqtt_client.connected()) {
+    reconnect();
   }
-  else
-  {
-    Serial.println("NÃO MOVIMENTO");
-  }
+  mqtt_client.loop(); //processa as mensagens e mantem a conexao com o broker MQTT
+  
+  Serial.println(analogRead(A0));
+  analogWrite(3, analogRead(A0));
+  Serial.println(analogRead(A0));
+  delay(1000); // Delay a little bit to improve simulation performance
 }
